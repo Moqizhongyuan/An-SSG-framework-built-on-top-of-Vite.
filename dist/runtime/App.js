75aed7acdde4779a02dfb3284b319917019d0a34
Zhongyuan Yu <zhongyuanyu@ZhongyuandeMacBook-Air.local>
2024-10-22 13:08:58 +0800

    HMR

---
+++ dist/runtime/App.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.App = App;
+const jsx_runtime_1 = require("react/jsx-runtime");
+const theme_default_1 = require("../theme-default");
+function App() {
+    return (0, jsx_runtime_1.jsx)(theme_default_1.Layout, {});
+}
