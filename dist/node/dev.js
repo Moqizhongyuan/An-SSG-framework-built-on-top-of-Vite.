554bfac567819ae4b75a892011c6e3620c549707
Zhongyuan Yu <zhongyuanyu@ZhongyuandeMacBook-Air.local>
2024-10-22 12:09:37 +0800

    添加indexHtml插件

--- dist/node/dev.js
+++ dist/node/dev.js
@@ -2,8 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createDevServer = createDevServer;
 const vite_1 = require("vite");
+const indexHtml_1 = require("./plugin-island/indexHtml");
 async function createDevServer(root = process.cwd()) {
     return (0, vite_1.createServer)({
         root,
+        plugins: [(0, indexHtml_1.pluginIndexHtml)()]
     });
 }
